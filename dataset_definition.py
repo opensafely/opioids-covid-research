3b309125be83bbe31431655e2b29b9dd4bc4e135
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2023-07-07 13:38:45 +0100

    Changes

--- dataset_definition.py
+++ dataset_definition.py
@@ -15,8 +15,8 @@ index_date = "2020-03-01"
 ### Define population ##########################
 dataset.define_population(
     (patients.age_on(index_date) >= 18) & (patients.age_on(index_date) < 110) \
-    & (patients.sex.contains("male") | patients.sex.contains("female")) \
-    & (patients.date_of_death >= index_date) 
+    & ((patients.sex == "male") | (patients.sex == "female")) \
+    & (patients.date_of_death >= index_date) \
     & (practice_registrations.for_patient_on(index_date).practice_pseudo_id.is_not_null())
 )
 
@@ -45,7 +45,7 @@ dataset.sex = patients.sex
 #         when(imd < 32844 * 1 / 10).then("1 (most deprived)"),
 #         when(imd < 32844 * 2 / 10).then("2"),
 #         when(imd < 32844 * 3 / 10).then("3"),
-#         when(imd < 32844 * 4 / 10).then("4"),        
+#         when(imd < 32844 * 4 / 10).then("4"),
 #         when(imd < 32844 * 5 / 10).then("5"),
 #         when(imd < 32844 * 6 / 10).then("6"),
 #         when(imd < 32844 * 7 / 10).then("7"),
@@ -54,6 +54,49 @@ dataset.sex = patients.sex
 #         when(imd >= 32844 * 9 / 10).then("10 (least deprived)"),
 #         default="unknown"
 # )
+
+# Ethnicity
+ethnicity16 = clinical_events.where(clinical_events.snomedct_code.is_in(codelists.ethnicity_codes_16)) \
+    .where(clinical_events.date.is_on_or_before(index_date))
+    .sort_by(clinical_events.date) \
+    .last_for_patient() \
+    .snomedct_code.to_category(codelists.ethnicity_codes_16)
+
+dataset.ethnicity16 = case(
+    when(ethnicity16 == "1").then("White - British"),
+    when(ethnicity16 == "2").then("White - Irish"),
+    when(ethnicity16 == "3").then("White - Other"),
+    when(ethnicity16 == "4").then("Mixed - White/Black Caribbean"),
+    when(ethnicity16 == "5").then("Mixed - White/Black African"),
+    when(ethnicity16 == "6").then("Mixed - White/Asian"),
+    when(ethnicity16 == "7").then("Mixed - Other"),
+    when(ethnicity16 == "8").then("Asian or Asian British - Indian"),
+    when(ethnicity16 == "9").then("Asian or Asian British - Pakistani"),
+    when(ethnicity16 == "10").then("Asian or Asian British - Bangladeshi"),
+    when(ethnicity16 == "11").then("Asian or Asian British - Other"),
+    when(ethnicity16 == "12").then("Black - Caribbean"),    
+    when(ethnicity16 == "13").then("Black - African"),
+    when(ethnicity16 == "14").then("Black - Other"),
+    when(ethnicity16 == "15").then("Other - Chinese"),
+    when(ethnicity16 == "16").then("Other - Other"),
+    default="Unknown"
+)
+
+ethnicity6 = clinical_events.where(clinical_events.snomedct_code.is_in(codelists.ethnicity_codes_6)) \
+    .where(clinical_events.date.is_on_or_before(index_date)) \
+    .sort_by(clinical_events.date) \
+    .last_for_patient() \
+    .snomedct_code.to_category(codelists.ethnicity_codes_6)
+
+dataset.ethnicity6 = case(
+    when(ethnicity6 == "1").then("White"),
+    when(ethnicity6 == "2").then("Mixed"),
+    when(ethnicity6 == "3").then("South Asian"),
+    when(ethnicity6 == "4").then("Black"),
+    when(ethnicity6 == "5").then("Other"),
+    when(ethnicity6 == "6").then("Not stated"),
+    default="Unknown"
+)
 
 # Practice region
 dataset.region = practice_registrations.for_patient_on(index_date).practice_nuts1_region_name
@@ -95,17 +138,17 @@ def has_med_event(codelist, where=True):
 dataset.opioid_any = has_med_event(codelists.opioid_codes) # Any opioid
 
 # By admin route
-dataset.oral_opioid_any = has_med_event(codelists.opioid_codes) # Oral opioid
-dataset.buc_opioid_any = has_med_event(codelists.opioid_codes) # Buccal opioid
-dataset.inh_opioid_any = has_med_event(codelists.opioid_codes) # Inhaled opioid
-dataset.rec_opioid_any = has_med_event(codelists.opioid_codes) # Rectal opioid
-dataset.par_opioid_any = has_med_event(codelists.opioid_codes) # Parenteral opioid
-dataset.trans_opioid_any = has_med_event(codelists.opioid_codes) # Transdermal opioid
-dataset.oth_opioid_any = has_med_event(codelists.opioid_codes) # Other admin route opioid
+dataset.oral_opioid_any = has_med_event(codelists.opioid_codes)  # Oral opioid
+dataset.buc_opioid_any = has_med_event(codelists.opioid_codes)  # Buccal opioid
+dataset.inh_opioid_any = has_med_event(codelists.opioid_codes)  # Inhaled opioid
+dataset.rec_opioid_any = has_med_event(codelists.opioid_codes)  # Rectal opioid
+dataset.par_opioid_any = has_med_event(codelists.opioid_codes)  # Parenteral opioid
+dataset.trans_opioid_any = has_med_event(codelists.opioid_codes)  # Transdermal opioid
+dataset.oth_opioid_any = has_med_event(codelists.opioid_codes)  # Other admin route opioid
 
 # By strength/type
-dataset.hi_opioid_any = has_med_event(codelists.hi_opioid_codes) # High dose opioid
-dataset.long_opioid_any = has_med_event(codelists.long_opioid_codes) # Long-acting opioid
+dataset.hi_opioid_any = has_med_event(codelists.hi_opioid_codes)  # High dose opioid
+dataset.long_opioid_any = has_med_event(codelists.long_opioid_codes)  # Long-acting opioid
 
 
 ### No. people with a new opioid prescription (2 year lookback) ######################
