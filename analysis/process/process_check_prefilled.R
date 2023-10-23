b70bb0c5b7a3a851366ee413fde96840ce05b59a
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2023-10-23 14:12:14 +0100

    Forgot to aggregate

--- analysis/process/process_check_prefilled.R
+++ analysis/process/process_check_prefilled.R
@@ -24,23 +24,27 @@ dir_create(here::here("output", "measures"), showWarnings = FALSE, recurse = TRU
 # Clean up measures datasets  #
 ###############################
 
-type <- read_csv(here::here("output", "measures", "measures_test_3.csv")) %>%
-  mutate(month = as.Date(interval_start, format="%Y-%m-%d"),
-         period = ifelse(month < as.Date("2020-03-01"), "Pre-COVID", 
-                         ifelse(month >= as.Date("2021-04-01"), "Recovery", "Lockdown")),
-         prefilled = ifelse(dmd_code = 1075511000001100, "Pre-filled", "Other")) %>%
-  dplyr::select(!c(interval_start, interval_end, ratio)) 
-
-write.csv(type, file = here::here("output", "timeseries", "ts_prefilled.csv"),
-          row.names = FALSE)
+# type <- read_csv(here::here("output", "measures", "measures_test_3.csv")) %>%
+#   mutate(month = as.Date(interval_start, format="%Y-%m-%d"),
+#          period = ifelse(month < as.Date("2020-03-01"), "Pre-COVID", 
+#                          ifelse(month >= as.Date("2021-04-01"), "Recovery", "Lockdown")),
+#          prefilled = ifelse(dmd_code = 1075511000001100, "Pre-filled", "Other")) %>%
+#   dplyr::select(!c(interval_start, interval_end, ratio)) %>%
+#   group_by(month, prefilled) %>%
+#   summarise(count = sum(numerator))
+# 
+# write.csv(type, file = here::here("output", "timeseries", "ts_prefilled.csv"),
+#           row.names = FALSE)
 
 
 type2 <- read_csv(here::here("output", "measures", "measures_test_3.csv")) %>%
   mutate(month = as.Date(interval_start, format="%Y-%m-%d"),
          period = ifelse(month < as.Date("2020-03-01"), "Pre-COVID", 
                          ifelse(month >= as.Date("2021-04-01"), "Recovery", "Lockdown")),
-         prefilled = ifelse(dmd_code == "1075511000001100", "Pre-filled", "Other")) %>%
-  dplyr::select(!c(interval_start, interval_end, ratio)) 
+         prefilled = ifelse(dmd_code %in% c("1075511000001100"), "Pre-filled", "Other")) %>%
+  dplyr::select(!c(interval_start, interval_end, ratio)) %>%
+  group_by(month, prefilled) %>%
+  summarise(count = sum(numerator))
 
 write.csv(type2, file = here::here("output", "timeseries", "ts_prefilled2.csv"),
           row.names = FALSE)
