9a9d77bf25ea47f8a59c492378f5559e97df82c0
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2023-10-27 14:12:46 +0100

    Check dmd codes

--- analysis/process/process_check_diamorphine.R
+++ analysis/process/process_check_diamorphine.R
@@ -24,23 +24,10 @@ dir_create(here::here("output", "measures"), showWarnings = FALSE, recurse = TRU
 # Clean up measures datasets  #
 ###############################
 
-dia <- read_csv(here::here("output", "measures", "measures_test.csv")) %>%
-  mutate(month = as.Date(interval_start, format="%Y-%m-%d"),
-         period = ifelse(month < as.Date("2020-03-01"), "Pre-COVID",
-                         ifelse(month >= as.Date("2021-04-01"), "Recovery", "Lockdown"))) %>%
-  dplyr::select(!c(interval_start, interval_end, ratio)) %>%
-  group_by(month, measure) %>%
-  summarise(count = sum(numerator)) %>%
-  mutate(count = case_when(count> 10 ~ round(count / 7) * 7))
-  
-
-write.csv(dia, file = here::here("output", "timeseries", "ts_diamorphine.csv"),
-          row.names = FALSE)
 
 
 
-
-dmd <- read_csv(here::here("output", "measures", "measures_test_3.csv")) %>%
+dmd <- read_csv(here::here("output", "measures", "measures_test_6.csv")) %>%
   mutate(month = as.Date(interval_start, format="%Y-%m-%d"),
          period = ifelse(month < as.Date("2020-03-01"), "Pre-COVID",
                          ifelse(month >= as.Date("2021-04-01"), "Recovery", "Lockdown"))) %>%
